d894e74832fff7f5dcb1ba6413e96a6207f472ed
Your Name <you@example.com>
2022-03-11 11:55:03 -0300

    Revert "Add files via upload"
    This reverts commit 184b461ce5427e93a5ffc2f0fe3184a0a1e63598.

--- FuBank LTDA/script/hidder.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("areaUser").style.display="none";
-document.getElementById("infoUser").style.display="none";
-document.getElementById("alerta").style.display="none";
-document.getElementById("interecoesEssenciais").style.display="none";
-document.getElementById("historico").style.display="none";
-document.getElementById("areaRegistro").style.display="none";
-document.getElementById("porEnquantoNada").style.display="none";
